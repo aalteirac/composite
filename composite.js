d0fd056d91dd355fc65847bb0e2343b36b78e04c
alteirac <anthony@alteirac.com>
2018-11-16 22:01:35 +0100

    First commit

---
+++ composite.js
@@ -0,0 +1,212 @@
+define([
+	'jquery',
+	'qlik',
+	'./properties',
+	'./interact',
+	'css!./css/styles.css'
+],
+function ($, qlik, props,interact, cssContent) {
+	var painted=false;
+	var btn;
+	var	app = qlik.currApp(this);
+	const obKey = 'obj';
+	var TEMPLATE = {
+		MASK: '<div class="qvobject mask viz-without-export"></div>',
+		QVOBJECT: '<div class="qvobject obj viz-without-export"></div>',
+		EXTENSION: '<div qv-extension class="composite"></div>',
+	};
+
+	function getId(id,obid,chid,opt){
+		return id +'-'+obid +'-'+chid+'-'+opt.noInteraction+"-"+opt.noSelections+"-"+opt.pix;
+	}
+	function renderObject(id,objId,chartId, options) {
+		var objContainer = $(TEMPLATE.QVOBJECT);
+		var mask=$(TEMPLATE.MASK);
+		objContainer.attr('obid', getId(id,objId,chartId,options));
+		mask.attr('obid', getId(id,objId,chartId,options));
+		objContainer.attr('chart-id', chartId);
+		mask.attr('chart-id', chartId);
+		objContainer.attr('obj-id', objId);
+		mask.attr('obj-id', objId);
+		app.getObject(objContainer,chartId, options);
+		return [mask,objContainer];
+	}
+
+	function save(me,layout){
+		me.backendApi.getProperties().then(function(reply){
+			reply.props=layout.props;
+			me.backendApi.setProperties(reply);
+		})
+	}
+
+
+	return {
+		initialProperties: {
+			version : 1.0,
+			activeTab: 1
+		},
+		definition: props,
+		support: {
+			export: false,
+			exportData: false,
+			snapshot: false
+		},
+		beforeDestroy: function () {
+			currActiveTab = null;
+
+			if (btn) {
+				btn.off();
+				btn.remove();
+				btn = null;
+			}
+		},
+		paint: function ($element, layout) {
+			var self = this;
+			var id = layout.qInfo.qId;
+			var props = layout.props;
+			var extElem = $(TEMPLATE.EXTENSION);
+			var mode=qlik.navigation.getMode();
+			extElem.attr("id",id);
+			function setDragResize(id,obKey,chartId,objectOptions){
+				interact(`.mask[obid='${getId(id,obKey,chartId,objectOptions)}']`).draggable({
+					snap: {
+						targets: [
+						  interact.createSnapGrid({ x: 5, y: 5 })
+						],
+						range: Infinity,
+						relativePoints: [ { x: 0, y: 0 } ]
+					  },
+					  inertia: true,
+					  restrict: {
+						restriction: $element[0],
+						elementRect: { top: 0, left: 0, bottom: 1, right: 1 },
+						endOnly: true
+					  },
+					onmove(event) {
+						var target = event.target,
+						x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx,
+						y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
+						target.style.webkitTransform =
+						target.style.transform ='translate(' + x + 'px, ' + y + 'px)';
+						target.setAttribute('data-x', x);
+						target.setAttribute('data-y', y);
+						$(`.obj[obid='${target.getAttribute("obid")}']`).css('transform','translate(' + x + 'px, ' + y + 'px)')
+					},
+					onend(event) {
+						var x=parseInt(event.target.getAttribute('data-x'));
+						var y=parseInt(event.target.getAttribute('data-y'));
+						var rx=x/$element.width();
+						var ry=y/$element.height();
+						layout.props[event.target.getAttribute("obj-id")].coord=rx+"#"+ry;
+						layout.props[event.target.getAttribute("obj-id")].rawCoord=x+"#"+y;
+						save(self,layout)
+					}
+				})
+				.resizable({
+					edges: { left:true, right: true, bottom: true, top:true},
+					restrictEdges: {
+					outer: $element[0],
+					endOnly: true,
+					},
+					restrictSize: {
+					min: { width: 20, height: 20 },
+					},		
+					inertia: true,
+				})
+				.on('resizemove', function (event) {
+					var target = event.target,
+						x = (parseFloat(target.getAttribute('data-x')) || 0),
+						y = (parseFloat(target.getAttribute('data-y')) || 0);
+						// translate when resizing from top or left edges
+					target.style.width  = event.rect.width + 'px';
+					target.style.height = event.rect.height + 'px';
+					x += event.deltaRect.left;
+					y += event.deltaRect.top;
+					target.style.webkitTransform = target.style.transform ='translate(' + x + 'px,' + y + 'px)';
+					target.setAttribute('data-x', x);
+					target.setAttribute('data-y', y);
+					$(`.obj[obid='${target.getAttribute("obid")}']`).css('transform','translate(' + x + 'px,' + y + 'px)');
+					$(`.obj[obid='${target.getAttribute("obid")}']`).css('width',target.style.width).css('height',target.style.height);
+					
+				}).on('resizeend', function (event) {
+					var target = event.target,
+						x = (parseFloat(target.getAttribute('data-x')) || 0),
+						y = (parseFloat(target.getAttribute('data-y')) || 0);
+					var w=$(target).width();
+					var h=$(target).height();
+					var rw=$(target).width()/$element.width();
+					var rh=$(target).height()/$element.height();
+					var rx=x/$element.width();
+					var ry=y/$element.height();
+					layout.props[event.target.getAttribute("obj-id")].coord=rx+"#"+ry;
+					layout.props[event.target.getAttribute("obj-id")].rawCoord=x+"#"+y;
+					layout.props[event.target.getAttribute("obj-id")].size=rw+"#"+rh;
+					layout.props[event.target.getAttribute("obj-id")].rawSize=w+"#"+h;
+					save(self,layout)
+					qlik.resize(`.obj[obid='${target.getAttribute("obid")}']`);
+				});
+			}
+			if($("#"+id).length===0){
+				$element.append(extElem);
+			}
+			for (var i = parseInt(props.objNumber)+1; i < props.maxObj; i++) {	//clean removed objects	
+				$("#"+id).find(`.obj[obj-id='${obKey+i}']`).remove();
+				$("#"+id).find(`.mask[obj-id='${obKey+i}']`).remove();
+				layout.props[obKey+i].chart="";
+				layout.props[obKey+i].coord="0#0";
+				layout.props[obKey+i].rawCoord="0#0";
+				layout.props[obKey+i].size="";
+				layout.props[obKey+i].rawSize="";
+				save(self,layout)
+			}
+			for (var i = 1; i <= props.objNumber; i++) {						//display objects
+				var chartId = props[obKey+i].chart;
+				var objectOptions = {
+					'noInteraction': props[obKey+i].interact,
+					'noSelections': props[obKey+i].select,
+					'pix':props[obKey+i].pix
+				};
+				var uniqSelector=`[obid='${getId(id,obKey+i,chartId,objectOptions)}']`;
+				var uniq=$(uniqSelector);
+				if(uniq.length===0){											//new or changed object
+					$("#"+id).find(`[obj-id='${obKey+i}']`).remove();
+					$("#"+id).append(renderObject(id,obKey+i,chartId, objectOptions));
+					if(!interact.isSet(`.mask[obid='${getId(id,obKey+i,chartId,objectOptions)}']`))
+						setDragResize(id,obKey+i,chartId,objectOptions);
+				}
+				if(props[obKey+i].coord!=""){									//set object coordinates
+					var x = $element.width() *  parseFloat(props[obKey+i].coord.split('#')[0]);
+					var y =  $element.height() *  parseFloat(props[obKey+i].coord.split('#')[1]);
+					if(layout.props[obKey+i].pix===true){
+						x = parseFloat(props[obKey+i].rawCoord.split('#')[0]);
+						y = parseFloat(props[obKey+i].rawCoord.split('#')[1]);
+					}
+					$("#"+id).find(uniqSelector).attr('data-x',x)
+						.attr('data-y',y)
+						.css('transform','translate(' + x + 'px, ' + y + 'px)');
+				}
+				if(props[obKey+i].size!=""){									//set object size
+					var w = $element.width() * parseFloat(props[obKey+i].size.split('#')[0]);
+					var h = $element.height() * parseFloat(props[obKey+i].size.split('#')[1]);
+					if(layout.props[obKey+i].pix===true){
+						w = parseFloat(props[obKey+i].rawSize.split('#')[0]);
+						h = parseFloat(props[obKey+i].rawSize.split('#')[1]);
+					}
+					$("#"+id).find(uniqSelector).css('width',w).css('height',h)
+				}
+				else{															//default size
+					var w=$("#"+id).find(uniqSelector).width();
+					var h=$("#"+id).find(uniqSelector).height();
+					var rw=$("#"+id).find(uniqSelector).width()/$element.width();
+					var rh=$("#"+id).find(uniqSelector).height()/$element.height();
+					layout.props[obKey+i].size=rw+"#"+rh;
+					layout.props[obKey+i].rawSize=w+"#"+h;
+					save(self,layout);
+				}
+				$('.mask').hide();
+				if(mode==='edit')												//enable drag and size in edit mode
+					$('.mask').show();
+			}
+		}
+	};
+});
---
+++ properties.js
@@ -0,0 +1,192 @@
+"use strict"
+define(["qlik"], function (qlik) {
+    var app = qlik.currApp(this),
+        permission = null,
+        masterObjectPromise;
+    var masterobjects;
+    const MAXOBJ=10;
+
+    function getItems(){
+        var im={
+            dropdown: {
+                ref: "props.objNumber",
+                type: "string",
+                label: function (data) {
+                    return 'Layer Number ('+data.props.objNumber+' on '+MAXOBJ+')';
+                },
+                defaultValue: "1",
+                component: "slider",
+                min: 1,
+                max: MAXOBJ,
+                step: 1
+            },
+            maxO:{
+                ref:  "props.maxObj",
+                type: "number",
+                defaultValue: MAXOBJ,
+                show:false
+            }
+        }
+        for (let i = 1; i <= MAXOBJ; i++) {
+            im['obj'+i]=
+                {
+                    type: "items",
+                    label: function (data) {
+                        return data.props["obj"+i].label;
+                    },
+                    show: function (data) {
+                        return data.props.objNumber >= i;//BUG DESKTOP
+                    },
+                    items: generateItemObject(i)
+                }
+        }
+        return im;
+    }
+    function exportAllowed() {
+        if (!permission) {
+            return app.getAppLayout().then(function (result) {
+                permission = result.layout.permissions.exportData;
+                return permission;
+            });
+        } else {
+            setTimeout(function () {
+                permission = null;
+            }, 1000);
+            return permission;
+        }
+    }
+
+    function getMasterObjectList () {
+        if (!masterObjectPromise) {
+            var defer = qlik.Promise.defer();
+            app.getAppObjectList('masterobject', function (data) {
+                masterobjects = data.qAppObjectList.qItems.map(function (item) {
+                    return {
+                        value: item.qInfo.qId,
+                        label: item.qMeta.title
+                    };
+                });
+                return defer.resolve(masterobjects);
+            });
+            masterObjectPromise = defer.promise;
+        }
+        return masterObjectPromise;
+    }
+
+    function generateItemObject(nbr) {
+
+        return {
+            obj: {
+                component: "header",
+                label: "Layer " + nbr
+            },
+            chart: {
+                ref: "props.obj" + nbr + ".chart",
+                type: "string",
+                component: "dropdown",
+                label: "Master Object",
+                defaultValue: "",
+                options: function () {
+                    if (masterobjects) {
+                        return masterobjects;
+                    }
+                    return getMasterObjectList();
+                }
+            },
+            export: {
+                ref: "props.obj" + nbr + ".export",
+                type: "boolean",
+                label: "Enable export",
+                defaultValue: false,
+                show: function (data) {
+                    return false;
+                    //return data.props["obj" + nbr].chart !== "" && exportAllowed();
+                }
+            },
+            responsive: {
+                ref: "props.obj" + nbr + ".pix",
+                type: "boolean",
+                label: "Disable Responsive",
+                defaultValue: false,
+                show: function (data) {
+                    return data.props["obj" + nbr].chart !== "";
+                }
+            },
+            interaction: {
+                ref: "props.obj" + nbr + ".interact",
+                type: "boolean",
+                label: "Disable Interaction",
+                defaultValue: false,
+                show: function (data) {
+                    return data.props["obj" + nbr].chart !== "" ;
+                }
+            },
+            selection: {
+                ref: "props.obj" + nbr + ".select",
+                type: "boolean",
+                label: "Disable Selection",
+                defaultValue: false,
+                show: function (data) {
+                    return data.props["obj" + nbr].chart !== "" ;
+                }
+            },
+            coord:{
+                ref: "props.obj" + nbr + ".coord", 
+                type: "string",
+                label: "obj coord",
+                defaultValue:"0#0",
+                show: false
+            },
+            rawCoord:{
+                ref: "props.obj" + nbr + ".rawCoord", 
+                type: "string",
+                label: "obj coord",
+                defaultValue:"0#0",
+                show: false
+            },
+            size:{
+                ref: "props.obj" + nbr + ".size", 
+                type: "string",
+                label: "obj size",
+                show:false
+            },
+            rawSize:{
+                ref: "props.obj" + nbr + ".rawSize", 
+                type: "string",
+                label: "obj size",
+                show:false
+            }
+        };
+    }
+
+    var settings = {
+        uses: "settings",
+        items: {
+            general: {
+                items: {
+                    showTitles: {
+                        defaultValue: false
+                    }
+                }
+            },
+            selections: {
+                show: false
+            },
+            objs: {
+                grouped: false,
+                type: "items",
+                label: "Layers",
+                items: getItems()
+            }
+        }
+    };
+
+
+    return {
+        type: "items",
+        component: "accordion",
+        items: {
+            settings: settings
+        }
+    };
+});
